3945b754e6377c8bea90397766f5d883dc4b5b55
Le Quang Trong Tai <letaii@Hotmail.com>
2018-09-01 00:13:43 +0700

    + add Player.cs

---
+++ SwitchColor/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jumpForce = 10.0f;
+    public Rigidbody2D circle;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            circle.velocity = Vector2.up * jumpForce;
+        }
+	}
+}
