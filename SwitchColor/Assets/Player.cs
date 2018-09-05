75567160446a0f25646e62ba436dfb4c3e8b4d95
Le Quang Trong Tai <letaii@Hotmail.com>
2018-09-05 23:21:21 +0700

    + Edit in Player.cs

--- SwitchColor/Assets/Player.cs
+++ SwitchColor/Assets/Player.cs
@@ -6,9 +6,15 @@ public class Player : MonoBehaviour {
 
     public float jumpForce = 10.0f;
     public Rigidbody2D circle;
+    public string currentColor;
+    public SpriteRenderer sr;
+    public Color blue;
+    public Color yellow;
+    public Color pink;
+    public Color purple;
 
     void Start() {
-
+        setRandomColor();
     }
 
     // Update is called once per frame
@@ -18,4 +24,35 @@ public class Player : MonoBehaviour {
             circle.velocity = Vector2.up * jumpForce;
         }
 	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+    }
+
+    void setRandomColor()
+    {
+        int rand = Random.Range(0, 3);
+        switch (rand)
+        {
+            case 0:
+                currentColor = "Blue";
+                sr.color = blue;    
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = yellow;
+                break;
+            case 2:
+                currentColor = "Pink";
+                sr.color = pink;
+                break;
+            case 3:
+                currentColor = "Purple";
+                sr.color = purple;
+                break;
+            default:
+                break;
+        }
+    }
 }
+ 
